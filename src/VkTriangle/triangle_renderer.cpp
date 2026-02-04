afc24c071c05f402cfa06c8158d596ae4e4ecd02
UMU618 <umu618@hotmail.com>
2026-02-04 17:46:09 +0800

    - setPreferredColorFormats
    这个 setPreferredColorFormats 是为了测试 DXKV 而加的，修复 DXVK bug 后，不需要了。

--- src/VkTriangle/triangle_renderer.cpp
+++ src/VkTriangle/triangle_renderer.cpp
@@ -25,8 +25,8 @@ inline VkDeviceSize Aligned(VkDeviceSize v, VkDeviceSize byte_align) {
 
 TriangleRenderer::TriangleRenderer(QVulkanWindow* w, bool msaa) noexcept
     : window_(w) {
-  w->setPreferredColorFormats(
-      {VK_FORMAT_B8G8R8A8_UNORM, VK_FORMAT_R8G8B8A8_UNORM});
+  // w->setPreferredColorFormats(
+  //     {VK_FORMAT_B8G8R8A8_UNORM, VK_FORMAT_R8G8B8A8_UNORM});
   if (msaa) {
     const QVector<int> counts = w->supportedSampleCounts();
     qDebug() << "Supported sample counts:" << counts;
